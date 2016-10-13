baba1e1a14df0f1ea35a1b1f44fc1acdb7e6670e
Thomas Mello <work.mello@gmail.com>
2016-10-14 01:40:25 +0300

    add continuous prop to occupations

--- src/cards/occupations.js
+++ src/cards/occupations.js
@@ -3,6 +3,7 @@ export let diplomat = {
   token: 'diplomat',
   disclosed: false,
   availability: false,
+  continuous: false,
   description: 'Один раз за игру и только в свой ход вы можете потребовать у другого игрока, чтобы он обменял вам определенный предмет. Если у игрока нет названного предмета, то ваш ход тут же заканчивается.'
 }
 
@@ -11,6 +12,7 @@ export let hypno = {
   token: 'hypno',
   disclosed: false,
   availability: false,
+  continuous: false,
   description: 'Если вы атакующий, то сразу после того, как кто-то заявил о своём желании поддержать одну из сторон, вы можете заставить его не вмешиваться в эту дуэль.'
 }
 
@@ -19,6 +21,7 @@ export let seer = {
   token: 'seer',
   disclosed: false,
   availability: false,
+  continuous: false,
   description: 'Один раз за игру и только в свой ход вы можете посмотреть колоду предметов и выбрать 2 из них. Затем перемешайте колоду и положите выбранные предметы в любом порядке сверху.'
 }
 
@@ -27,6 +30,7 @@ export let doctor = {
   token: 'doctor',
   disclosed: false,
   availability: false,
+  continuous: false,
   description: 'Один раз за игру, сразу после дуэли, вы можете отменить результат этой дуэли.'
 }
 
@@ -35,6 +39,7 @@ export let priest = {
   token: 'priest',
   disclosed: false,
   availability: false,
+  continuous: false,
   description: 'Один раз за игру, до того как другие заявили, кого они поддерживают, вы можете предотвратить дуэль. Если атакующий имеет хотя бы 2 предмета, он должен отдать вам один предмет по своему выбору. Ход атакующего тут же заканчивается.'
 }
 
@@ -43,6 +48,7 @@ export let guard = {
   token: 'guard',
   disclosed: false,
   availability: false,
+  continuous: true,
   description: 'Если вы поддерживаете игрока, он получает +1.'
 }
 
@@ -51,6 +57,7 @@ export let alchemist = {
   token: 'alchemist',
   disclosed: false,
   availability: false,
+  continuous: false,
   description: 'Один раз за игру вы можете назначить победителя дуэли, но только если вы сами не являетесь атакующим или защищающимся в дуэли.'
 }
 
@@ -59,6 +66,7 @@ export let thug = {
   token: 'thug',
   disclosed: false,
   availability: false,
+  continuous: true,
   description: 'Вы можете добавить себе +1 в дуэли, если вы атакующий.'
 }
 
@@ -67,6 +75,7 @@ export let duelist = {
   token: 'duelist',
   disclosed: false,
   availability: false,
+  continuous: false,
   description: 'Один раз за игру, если вы атакуете или защищаетесь, вы можете заявить, что никто не должен оказывать поддержку в этой дуэли. Также вы получаете +1 в этой дуэли.'
 }
 
@@ -75,5 +84,6 @@ export let master = {
   token: 'master',
   disclosed: false,
   availability: false,
+  continuous: true,
   description: 'Вы можете добавить +1 в дуэли, если вы защищающийся.'
 }
