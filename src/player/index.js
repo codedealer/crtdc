1d88b27c01c197657db6b27fe149ccaa6abe0729
Thomas Mello <work.mello@gmail.com>
2016-11-08 21:42:08 +0300

    add character to player constructor

--- src/player/index.js
+++ src/player/index.js
@@ -1,17 +1,9 @@
 import Status from './status'
-// import {brotherhood} from '../cards/allegiance'
-// import {diplomat} from '../cards/occupations'
-// import characters from '../cards/characters.json'
 
 export default class Player {
-  constructor (uid, status = Status.IDLE) {
+  constructor (uid, status = Status.IDLE, character = false) {
     this.uid = uid;
     this.status = status;
-    // this.occupation = Object.assign({}, diplomat);
-    // this.allegiance = Object.assign({}, brotherhood);
-    // this.hand = [];
-    // this.tokens = 0;
-    // this.known = [];
-    // this.character = characters[0];
+    if (character) this.character = character;
   }
 }
